4437f5bd798eb14a0fcdbd9ed52bf20044d0e201
Janna <jannahakkarainen@hotmail.com>
2024-02-28 22:59:30 +0100

    stores are displayed in a html document with links

--- public/start.js
+++ public/start.js
@@ -1,10 +1,30 @@
-console.log('Hello')
-
 fetch("http://localhost:3005/stores")
 .then(response => response.json())
 .then(stores => {
-
-    console.log(stores)
+    const storeListContainer = document.getElementById('storeList');
     
-    
+    stores.forEach(store => {
+        if (store.url !== null) {
+            const storeLink = document.createElement('a');
+            storeLink.textContent = store.name;
+            
+            if (store.url && !store.url.startsWith("http://") && !store.url.startsWith("https://")) {
+                storeLink.href = "http://" + store.url;
+            } else {
+                storeLink.href = store.url;
+            }
+            
+            storeLink.target = "_blank"; 
+            
+            storeListContainer.appendChild(storeLink);
+        } else {
+            const storeName = document.createElement('span');
+            storeName.textContent = store.name;
+            storeListContainer.appendChild(storeName);
+        }
+        storeListContainer.appendChild(document.createElement('br'));
+    });
 })
+.catch(error => {
+    console.error('Error fetching stores:', error);
+});
