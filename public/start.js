d4b8209ce80ed083291c577471f8618c033941f4
Immie <emilia.zec@gmail.com>
2024-03-05 15:37:56 +0100

    Navigation bar is fixed in place

--- public/start.js
+++ public/start.js
@@ -60,3 +60,16 @@ function displayStores(stores) {
 }
 
 document.getElementById('applyFiltersButton').addEventListener('click', applyFilters);
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+
+        window.scrollTo({
+            top: target.offsetTop - 100,
+            behavior: 'smooth'
+        });
+    });
+});
